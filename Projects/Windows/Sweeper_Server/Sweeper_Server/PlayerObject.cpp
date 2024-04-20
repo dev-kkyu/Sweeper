3e6869c44190bb6d01466a4a4ca40b24d05dfade
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-04-20 16:44:54 +0900

    키 입력에 따른 플레이어 이동을 좀 더 보기 편하게 수정

--- Projects/Windows/Sweeper_Server/Sweeper_Server/PlayerObject.cpp
+++ Projects/Windows/Sweeper_Server/Sweeper_Server/PlayerObject.cpp
@@ -24,37 +24,40 @@ bool PlayerObject::update(float elapsedTime)
 	if (!keyState and !runJump)		// 업데이트 할 것이 없으면 리턴한다.
 		return false;
 
-	if ((not isFixedState and keyState) or (runJump and keyState)) {
-		glm::vec3 look = getLook();
-		glm::vec3 right = getRight();
+	// 공격중이 아니어야 XZ평면으로 움직일 수 있다. 다만 점프중일땐 움직일 수 있다
+	if (keyState) {
+		if (not isFixedState or runJump) {
+			glm::vec3 look = getLook();
+			glm::vec3 right = getRight();
 
-		glm::vec3 direction{ 0.f };
-		if (keyState & KEY_UP) direction += 1.f * look;
-		if (keyState & KEY_DOWN) direction -= 1.f * look;
-		if (keyState & KEY_LEFT) direction += 1.f * right;
-		if (keyState & KEY_RIGHT) direction -= 1.f * right;
+			glm::vec3 direction{ 0.f };
+			if (keyState & KEY_UP) direction += 1.f * look;
+			if (keyState & KEY_DOWN) direction -= 1.f * look;
+			if (keyState & KEY_LEFT) direction += 1.f * right;
+			if (keyState & KEY_RIGHT) direction -= 1.f * right;
 
-		// 점프 처리
-		if (keyState & KEY_SPACE) {
-			if (!runJump) {
-				velocity = jumpSpeed;
-				runJump = true;
+			// 점프 처리
+			if (keyState & KEY_SPACE) {
+				if (!runJump) {
+					velocity = jumpSpeed;
+					runJump = true;
+				}
 			}
-		}
 
-		move(direction, elapsedTime * 4.f);		// 초당 이동속도 4m
+			move(direction, elapsedTime * 4.f);		// 초당 이동속도 4m
 
-		// 이동 후 충돌처리
-		// 플레이어끼리
-		for (int i = 0; i < 4; ++i) {
-			if (i == my_id)
-				continue;
-			if (parentRoom->sessions[i]) {
-				if (isCollide(*parentRoom->sessions[i]->player)) {
-					glm::vec3 myPos = getPosition();
-					glm::vec3 otherPos = parentRoom->sessions[i]->player->getPosition();
-					glm::vec3 dir = myPos - otherPos;
-					move(dir, elapsedTime * 4.f);	// 움직인 방향과 무관하게, 상대와 나의 방향벡터를 구하면 슬라이딩 벡터가 가능하다
+			// 이동 후 충돌처리
+			// 플레이어끼리
+			for (int i = 0; i < 4; ++i) {
+				if (i == my_id)
+					continue;
+				if (parentRoom->sessions[i]) {
+					if (isCollide(*parentRoom->sessions[i]->player)) {
+						glm::vec3 myPos = getPosition();
+						glm::vec3 otherPos = parentRoom->sessions[i]->player->getPosition();
+						glm::vec3 dir = myPos - otherPos;
+						move(dir, elapsedTime * 4.f);	// 움직인 방향과 무관하게, 상대와 나의 방향벡터를 구하면 슬라이딩 벡터가 가능하다
+					}
 				}
 			}
 		}
