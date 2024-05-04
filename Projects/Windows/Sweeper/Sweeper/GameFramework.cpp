9c3d5c21d1057593d81c1937b94d9f63c7d60cdd
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-05-05 04:52:45 +0900

    배경 색상 (클리어 색상) 변경
    짙은 고동색으로 변경

--- Projects/Windows/Sweeper/Sweeper/GameFramework.cpp
+++ Projects/Windows/Sweeper/Sweeper/GameFramework.cpp
@@ -955,7 +955,7 @@ void GameFramework::recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t 
 		renderPassInfo.renderArea.extent = swapChainExtent;
 
 		std::array<VkClearValue, 2> clearValues{};
-		clearValues[0].color = { {0.0f, 0.0f, 0.0f, 1.0f} };
+		clearValues[0].color = { {30.f / 255 / 10.f, 10.f / 255 / 10.f, 0.f, 1.0f} };
 		clearValues[1].depthStencil = { 1.0f, 0 };
 
 		renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
