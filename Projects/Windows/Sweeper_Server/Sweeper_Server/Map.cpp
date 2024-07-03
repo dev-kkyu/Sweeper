96375d3c73af76db4235a692cc634f532596421a
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-07-03 18:32:08 +0900

    서버에 맵 충돌을 위한 맵 클래스 생성 완료

--- Projects/Windows/Sweeper_Server/Sweeper_Server/Map.cpp
+++ Projects/Windows/Sweeper_Server/Sweeper_Server/Map.cpp
@@ -1,1 +1,39 @@
 #include "Map.h"
+
+#define MAP_NAME "../../../../Projects/Windows/Sweeper/Sweeper/Models/map.glb"
+
+Map::Map()
+	: model{ MAP_NAME }
+{
+	// 바운딩 박스를 복사해 온다
+	for (const auto& node : this->model.nodes) {
+		copyBoundingBoxByNode(node);
+	}
+}
+
+Map::~Map()
+{
+}
+
+Map& Map::getInstance()
+{
+	static Map instance;		// C++11 이후 Thread-Safe 하다.
+	return instance;
+}
+
+const std::vector<BoundingBox>& Map::getBoundingBox() const
+{
+	return boundingBox;
+}
+
+void Map::copyBoundingBoxByNode(const std::shared_ptr<GLTFCollisionModel::Node> node)
+{
+	for (const auto& box : node->mesh.boundingBox) {
+		if (box.getTop() > 0.f)		// 아래쪽 박스는 사용하지 않는다.
+			boundingBox.push_back(box);
+	}
+
+	for (const auto& child : node->children) {
+		copyBoundingBoxByNode(child);
+	}
+}
--- Projects/Windows/Sweeper_Server/Sweeper_Server/Map.h
+++ Projects/Windows/Sweeper_Server/Sweeper_Server/Map.h
@@ -1,5 +1,30 @@
 #pragma once
+
+#include "GLTFCollisionModel.h"
+
+// 싱글톤 클래스
 class Map
 {
+private:
+	const GLTFCollisionModel model;
+
+	std::vector<BoundingBox> boundingBox;
+
+private:			// 싱글톤으로 만들기 위해 생성자 외부 노출 X
+	Map();
+	~Map();
+
+	// 복사는 할 일 없으니 막아둔다.	// 싱글톤을 위해서도 막아준다.
+	Map(const Map&) = delete;
+	Map& operator=(const Map&) = delete;
+
+public:
+	static Map& getInstance();	// 싱글톤 객체 얻기 위한 static 함수
+
+	const std::vector<BoundingBox>& getBoundingBox() const;
+
+private:
+	void copyBoundingBoxByNode(const std::shared_ptr<GLTFCollisionModel::Node> node);
+
 };
 
