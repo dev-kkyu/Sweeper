4037233d0b5daa369724403180b2d79a274abe31
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-07-18 20:22:05 +0900

    힐러 충돌처리 부분 힐 하는것으로 변경

--- Projects/Windows/Sweeper_Server/Sweeper_Server/HealerObject.cpp
+++ Projects/Windows/Sweeper_Server/Sweeper_Server/HealerObject.cpp
@@ -1,6 +1,7 @@
 #include "HealerObject.h"
 
 #include "Room.h"
+#include "Session.h"
 #include "MonsterObject.h"
 #include <iostream>
 
@@ -67,15 +68,17 @@ void HealerSKILLState::update(float elapsedTime)
 		player.changeIDLEState();
 	}
 	else if (now_time > stateBeginTime + std::chrono::milliseconds{ 200 }) {
-		// 충돌검사
-		auto myPos = player.getPosition();
-		myPos += player.getLook();	// 칼 범위를 플레이어 앞쪽으로 세팅해준다.
-		for (auto& m : player.parentRoom->monsters) {
-			auto monPos = m.second->getPosition();
-			float dist2 = (myPos.x - monPos.x) * (myPos.x - monPos.x) + (myPos.z - monPos.z) * (myPos.z - monPos.z);
-			if (dist2 <= 1.5f) {	// 충돌
-				m.second->onHit(player);
-				std::cout << m.first << ": 몬스터 공격받음" << std::endl;
+		// 팀원 체력 회복
+		glm::vec3 spawnPos = player.getPosition() + player.getLook() * 5.5f;		// 스킬 범위 지정
+		for (int i = 0; i < player.parentRoom->sessions.size(); ++i) {				// 힐러도 힐이 될 수 있다.
+			std::shared_ptr<Session> session = player.parentRoom->sessions[i].load();
+			if (Room::isValidSession(session)) {
+				auto playerPos = session->player->getPosition();
+				float dist2 = glm::pow(spawnPos.x - playerPos.x, 2.f) + glm::pow(spawnPos.z - playerPos.z, 2.f);
+				float range2 = glm::pow(3.f, 2.f);		// 반지름 3 내의 범위에 있는지
+				if (dist2 <= range2) {
+					// Todo 체력 회복, 이 코드를 player의 update로 옮겨야 한다
+				}
 			}
 		}
 	}
